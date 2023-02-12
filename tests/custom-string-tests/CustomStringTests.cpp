f16bcb249e38bf9b66e758ad68d6ee8b7cd8102d
Volkov Daniil <volkov.study@mail.ru>
2023-02-12 18:09:40 +0300

    Add CustomString test: OutputOperator / HandlesString

--- tests/custom-string-tests/CustomStringTests.cpp
+++ tests/custom-string-tests/CustomStringTests.cpp
@@ -1,3 +1,12 @@
 #include "gtest/gtest.h"
 
 #include "custom-string/CustomString.h"
+
+TEST(OutputOperator, HandlesString) {
+    CustomString customString("abcd");
+
+    stringstream output;
+    output << customString;
+
+    ASSERT_EQ(output.str(), "dcba");
+}
